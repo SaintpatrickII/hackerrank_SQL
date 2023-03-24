55ea05a581639e8435b7c2dc73cd3f1994e3be8d
Patrick Govus <patrickgovus99@gmail.com>
2023-03-24 12:16:40 +0000

    25 questions done

---
+++ Basic/Revising Aggregations - The Count Function.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(NAME) C FROM CITY
+WHERE POPULATION >100000;
+
+-- Alias count of cities as C for good practice :)
---
+++ Basic/Type of Triangle.sql
@@ -0,0 +1,10 @@
+SELECT CASE
+WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+WHEN A = B AND B = C THEN 'Equilateral'
+WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+ELSE 'Scalene'
+END
+FROM TRIANGLES;
+
+-- CASE STATEMENT, use when to start condition and THEN to put desired output
+-- CASE requires END at end of expression
